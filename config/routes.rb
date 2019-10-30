ac0197f8d5f520dfd9ca03b71e09d9adf96d4fae
Nolan Morache <nolan241@gmail.com>
2019-10-29 18:46:17 -0700

    create signuo scss and add partials import to application.scss

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 	devise_scope :user do
 		get 'login', to: 'devise/sessions#new'
 	end
-
+	
+	#Update path users/sign_up to signup
+	devise_scope :user do
+		get 'signup', to: 'devise/registrations#new'
+	end	
+	
 end
 
 
