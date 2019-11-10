765c3f5f60cc381c87f93bcb7bcdfb56b6cfb6c0
Nolan Morache <nolan241@gmail.com>
2019-11-10 14:44:12 -0800

    added nav stylying

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,13 @@ module Collabfield
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+		
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+		
+		#do not load all helper files to all views
+		config.action_controller.include_all_helpers = false
   end
 end
