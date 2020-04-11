0ecafe8969c41b33a43951f087e138f2b489fad7
Nolan Morache <nolan241@gmail.com>
2020-04-11 10:53:05 -0700

    Infinite Scroll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,30 @@ class PostsController < ApplicationController
 
 	private
 
-  def get_posts
-  	Post.limit(30)
+	def get_posts
+	  branch = params[:action]
+	  search = params[:search]
+	  category = params[:category]
+
+	  if category.blank? && search.blank?
+	    posts = Post.by_branch(branch).all
+	  elsif category.blank? && search.present?
+	    posts = Post.by_branch(branch).search(search)
+	  elsif category.present? && search.blank?
+	    posts = Post.by_category(branch, category)
+	  elsif category.present? && search.present?
+	    posts = Post.by_category(branch, category).search(search)
+	  else
+	  end
 	end
 
 	def posts_for_branch(branch)
 	  @categories = Category.where(branch: branch)
 	  @posts = get_posts.paginate(page: params[:page])
+	  respond_to do |format|
+	  format.html
+	  format.js { render partial: 'posts/posts_pagination_page' }
+		end
 	end
 
 end
