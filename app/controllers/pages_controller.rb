de5477ff6e1e74643ba89d5b328b57243c455903
Nolan Morache <nolan241@gmail.com>
2020-04-12 11:58:05 -0700

    Create service object to  def posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
-    
     def index
     	@posts = Post.limit(5)
 	    @hobby_posts = Post.by_branch('hobby').last(8)
 	    @study_posts = Post.by_branch('study').last(8)
 	    @team_posts = Post.by_branch('team').last(8)
     end
-    
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,22 +17,13 @@ class PostsController < ApplicationController
 
 	private
 
-	def get_posts
-	  branch = params[:action]
-	  search = params[:search]
-	  category = params[:category]
-
-	  if category.blank? && search.blank?
-	    posts = Post.by_branch(branch).all
-	  elsif category.blank? && search.present?
-	    posts = Post.by_branch(branch).search(search)
-	  elsif category.present? && search.blank?
-	    posts = Post.by_category(branch, category)
-	  elsif category.present? && search.present?
-	    posts = Post.by_category(branch, category).search(search)
-	  else
-	  end
-	end
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
 
 	def posts_for_branch(branch)
 	  @categories = Category.where(branch: branch)
