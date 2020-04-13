7b9d228c392708e8ddaeac883fbbf58d0a355a12
Nolan Morache <nolan241@gmail.com>
2020-04-12 22:16:00 -0700

    Create new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+	before_action :redirect_if_not_signed_in, only: [:new]
+	
 	def show
 		@post = Post.find(params[:id])
 	end
@@ -15,8 +18,28 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
-	private
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end	
+	
+private
+
+	def post_params
+		params.require(:post).permit(:content, :title, :category_id)
+												 .merge(user_id: current_user.id)
+	end	
+	
   def get_posts
     PostsForBranchService.new({
       search: params[:search],
